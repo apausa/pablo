251ee61aaf0ab74386ca423b8a2f8be84570af44
pabloapausa <pabloapausa@outlook.com>
2021-09-28 10:54:55 +0200

    Organize all routes

--- pablo/pages/_app.tsx
+++ pablo/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import '../styles.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
---
+++ pablo/pages/designs/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Designs() {
+  return (
+    <main>
+      <h1>Designs</h1>
+    </main>
+  );
+}

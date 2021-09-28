07d8ef37698ed666d5ffc949d28ffa0f441a492f
pabloapausa <pabloapausa@outlook.com>
2021-09-28 10:35:37 +0200

    Start mock render

---
+++ pablo/pages/design/times/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Times() {
+  return (
+    <main>
+      <h1>Times New Roman</h1>
+    </main>
+  );
+}
--- pablo/pages/index.tsx
+++ pablo/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
-  <div className={styles.container}>
+  <main>
     <h1>Pablo</h1>
-  </div>
+  </main>
 );
 
 export default Home;

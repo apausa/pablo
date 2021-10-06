de44bb285173d2dfbd88363bb3d12ab1f61076cf
pabloapausa <pabloapausa@outlook.com>
2021-10-06 19:00:31 +0200

    Define Times content structure

--- pablo/pages/pablo/typography/times/index.tsx
+++ pablo/pages/pablo/typography/times/index.tsx
@@ -6,22 +6,9 @@ export default function Times() {
       <h2>Times New Roman.</h2>
       <section>
         <h3>Clasification.</h3>
-        <article>
-          <p>Style:Serif.</p>
-          <p>Category: Transitional.</p>
-          <p>Family: Regular.</p>
-        </article>
       </section>
       <section>
         <h3>History.</h3>
-        <article>
-          <p>Release: 1932.</p>
-          <p>Foundry: Monotype.</p>
-          <p>Commissioner: The Times.</p>
-          <p>License: Propietary.</p>
-          <p>Designers: Stanley Morison and Victor Lardent.</p>
-          <p>Original: Plantin.</p>
-        </article>
       </section>
       <section>
         <h3>Analysis</h3>

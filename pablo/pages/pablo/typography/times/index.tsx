8b19999b7bcb7df09be0316989c818ca42b17cd4
pabloapausa <pabloapausa@outlook.com>
2021-10-06 15:32:45 +0200

    Add clasification

---
+++ pablo/pages/pablo/typography/times/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Times() {
+  return (
+    <main>
+      <h2>Times New Roman.</h2>
+      <section>
+        <h3>Clasification</h3>
+        <article>
+          <p>Style: Serif.</p>
+          <p>Category: Transitional.</p>
+          <p>Family: Regular.</p>
+        </article>
+      </section>
+      <section>
+        <h3>History</h3>
+      </section>
+      <section>
+        <h3>Analysis</h3>
+      </section>
+      <section>
+        <h3>Examples</h3>
+      </section>
+      <section>
+        <h3>Others</h3>
+      </section>
+    </main>
+  );
+}

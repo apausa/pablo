1aab26eee16029e0dcddf3a3c303189c1833e52a
pabloapausa <pabloapausa@outlook.com>
2021-10-24 21:44:00 +0200

    Populate Head tag

--- project/pages/_app.tsx
+++ project/pages/_app.tsx
@@ -1,9 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Pablo Apausa</title>
+        <meta name="description" content="Pablo Apausa" />
+        <meta property="og:title" content="Pablo Apausa" />
+        <meta property="og:description" content="Pablo Apausa" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:url" content="https://apausa.dev/" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;

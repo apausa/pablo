09ede57dd953394d0a87e51c40e1c15147a45698
pabloapausa <pabloapausa@outlook.com>
2021-10-24 20:56:14 +0200

    Delete

--- pablo/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { NextPage } from 'next';
-import styles from '../styles/Index.module.scss';
-
-const Home: NextPage = () => (
-  <main className={styles.background} />
-);
-
-export default Home;
--- pablo/pages/pablo/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Designs() {
-  return (
-    <main>
-      <h2>Designs</h2>
-    </main>
-  );
-}

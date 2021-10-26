56f965eb74ebbbc0208f672ecb96be8c323c77ae
pabloapausa <pabloapausa@outlook.com>
2021-10-26 21:11:58 +0200

    Style Header component

--- project/components/Header.tsx
+++ project/components/Header.tsx
@@ -1,15 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Header.module.scss';
 
-const Header: NextPage = () => (
-  <header className={styles.header}>
-    <hgroup className={styles.header__top}>
-      <h1 className={styles.top__title}>Pablo Apausa.</h1>
-      <h2 className={styles.top__description}>Designer, Fullstack Developer.</h2>
-      <h2 className={styles.top__contact}>Contact: pablo@apausa.dev.</h2>
-    </hgroup>
-  </header>
-);
-
-export default Header;
+export default function Header() {
+  const { route } = useRouter();
+  const href = (route === '/') ? '/portfolio' : '/';
+  return (
+    <header className={styles.header}>
+      <hgroup className={styles.header__top}>
+        <h1 className={styles.top__title}>
+          <Link href={href}>
+            <a className={styles.title__link}>
+              Pablo Apausa.
+            </a>
+          </Link>
+        </h1>
+        <h2 className={styles.top__description}>Designer, Fullstack Developer.</h2>
+        <h2 className={styles.top__contact}>Contact: pablo@apausa.dev.</h2>
+      </hgroup>
+    </header>
+  );
+}

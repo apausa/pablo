96f6ec89f01f847756bf756bd5db0a4b128db4aa
pabloapausa <pabloapausa@outlook.com>
2021-11-02 16:35:29 +0100

    Structure HTML links

--- project/pages/index.tsx
+++ project/pages/index.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import {
+  BsLinkedin, BsGithub, BsTwitter,
+} from 'react-icons/bs';
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => (
-  <main className={styles.home} />
+  <main className={styles.home}>
+    <ul className={styles.home__social}>
+      <li>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/apausa/"
+        >
+          <BsLinkedin />
+        </a>
+      </li>
+      <li>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://github.com/apausa"
+        >
+          <BsGithub />
+        </a>
+      </li>
+      <li>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          href="https://twitter.com/pabloapausa"
+        >
+          <BsTwitter />
+        </a>
+      </li>
+    </ul>
+  </main>
 );
 
 export default Home;

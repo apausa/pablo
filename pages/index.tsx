96a9129d556d8d452d6ad7ed9b46352ca1423442
pabloapausa <pabloapausa@outlook.com>
2021-12-08 00:51:09 +0100

    Remove header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import type { NextPage } from 'next';
+import Link from 'next/link';
+
+import information from '../utils/information';
 import styles from '../styles/Home.module.scss';
 
-const Home: NextPage = () => (
-  <main className={styles.home} />
-);
-
-export default Home;
+export default function Home() {
+  const { name, description, contact } = information;
+  return (
+    <main className={styles.home}>
+      <ul>
+        <li>
+          <Link href="/">
+            <h1>
+              {name}
+            </h1>
+          </Link>
+        </li>
+        <li>
+          <h2>
+            {description}
+          </h2>
+        </li>
+        <li>
+          <Link href={`mailto:${contact}`}>
+            <h2>
+              {contact}
+            </h2>
+          </Link>
+        </li>
+      </ul>
+    </main>
+  );
+}
---
+++ utils/information.ts
@@ -0,0 +1,5 @@
+export default {
+  name: 'Pablo Apausa',
+  description: 'Designer, Frontend Developer',
+  contact: 'pablo@apausa.io',
+};

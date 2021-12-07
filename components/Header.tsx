d07c6bc618acb74ffd4d45b28dd9feab0b52221d
pabloapausa <pabloapausa@outlook.com>
2021-12-08 00:35:44 +0100

    Hardcoded TypeScript file

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,30 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Link from 'next/link';
 
 import styles from '../styles/Header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <hgroup className={styles.header__top}>
-        <h1 className={styles.top__title}>Pablo Apausa.</h1>
-        <h2 className={styles.top__description}>Product Designer, Frontend Developer.</h2>
-      </hgroup>
+      <ul>
+        <li>
+          <Link href="/">
+            <h1 className={styles.top__title}>
+              Pablo Apausa.
+            </h1>
+          </Link>
+        </li>
+        <li>
+          <h2 className={styles.top__description}>
+            Designer, Frontend Developer.
+          </h2>
+        </li>
+        <li>
+          <Link href="mailto:pablo@apausa.io">
+            <h2>pablo@apausa.io</h2>
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 }

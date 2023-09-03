b6672f0a83c92b757a75485ac692069722ce1afa
Pablo Apausa <pabloapausa@outlook.com>
2023-09-03 16:42:15 +0200

    Configure NextUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import { Providers } from './providers';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
